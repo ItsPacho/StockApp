58af36564e7005a88e688605cde4adb9f32fdb0d
ItsPacho <jesperwinsten@gmail.com>
2018-02-14 13:42:37 +0200

    Start of Project

---
+++ src/view/StockView.java
@@ -0,0 +1,18 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class StockView extends JFrame {
+
+	JFrame frame = new JFrame();
+
+	public StockView() {
+
+		frame.setTitle("Basic Window");
+		frame.setSize(500, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+}
